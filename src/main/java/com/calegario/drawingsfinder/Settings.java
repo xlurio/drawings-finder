5b9cb4ca6fc07ba39d2d6dc5864b6210b02b3181
Lucas Calegario <74337880+xlurio@users.noreply.github.com>
2022-05-08 18:17:57 -0300

    Update Settings.java

--- src/main/java/com/calegario/drawingsfinder/Settings.java
+++ src/main/java/com/calegario/drawingsfinder/Settings.java
@@ -1,5 +1,5 @@
 package com.calegario.drawingsfinder;
 
 public class Settings {
-    public static final String CSV_PATH = "/home/xlurio/Documents/paths.csv";
+    public static final String CSV_PATH = "";
 }
