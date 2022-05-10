a45d33620bb169f4e1938f30eb7f5da9a7784931
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-05-10 10:46:09 -0300

    CSV_PATH changed

--- src/main/java/com/calegario/drawingsfinder/App.java
+++ src/main/java/com/calegario/drawingsfinder/App.java
@@ -3,29 +3,38 @@ package com.calegario.drawingsfinder;
 import com.calegario.defaultwins.inputbox.InputBox;
 import com.calegario.csvdb.CSVDBManager;
 import com.calegario.searchfield.SearchListener;
-
 import java.io.*;
 import java.util.*;
+import javax.swing.JOptionPane;
 
 public class App
 {
     public static void main( String[] args ) throws FileNotFoundException, IOException
     {
-        String[] header = new String[]{
-            "file_name", "extension", "path", "last_mod"
-        };
-        CSVDBManager manager = new CSVDBManager(Settings.CSV_PATH,
-                                                header,
-                                                ';');
-        List<String[]> drawList = manager.getDB();
-        InputBox searchField = new InputBox(
-            "Procurar desenhos",
-            "Insira a referência do desenho que deseja encontrar:",
-            true,
-            352,
-            160
-        );
-        searchField.setBtnListener(new SearchListener(searchField, manager));
-        searchField.showBox();
+        if (!Settings.CSV_PATH.isEmpty()) {
+            String[] header = new String[]{
+                "file_name", "extension", "path", "last_mod"
+            };
+            CSVDBManager manager = new CSVDBManager(Settings.CSV_PATH,
+                                                    header,
+                                                    '|');
+            List<String[]> drawList = manager.getDB();
+            InputBox searchField = new InputBox(
+                "Procurar desenhos",
+                "Insira a referência do desenho que deseja encontrar:",
+                true,
+                352,
+                160
+            );
+            searchField.setBtnListener(new SearchListener(searchField, manager));
+            searchField.showBox();
+        } else {
+            JOptionPane.showMessageDialog(
+                null,
+                "CSV_PATH não configurado",
+                "ERRO",
+                JOptionPane.ERROR_MESSAGE
+            );
+        }
     }
 }
--- src/main/java/com/calegario/drawingsfinder/Settings.java
+++ src/main/java/com/calegario/drawingsfinder/Settings.java
@@ -1,5 +1,7 @@
 package com.calegario.drawingsfinder;
 
+import java.io.File;
+
 public class Settings {
-    public static final String CSV_PATH = "";
+    public static final String CSV_PATH = (new File("resources/drawingspath.csv").getAbsolutePath());
 }
